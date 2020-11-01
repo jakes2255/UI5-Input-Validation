ebe1128ee2ab15506c33d8d2e53bbf1cf311b0da
Jakes2255 <aj727in@gmail.com>
2020-11-01 14:37:44 +0000

    updates

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller,MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.demo.aj.Z_Input_Validation.controller.View1", {
@@ -12,6 +13,25 @@ sap.ui.define([
 			var mobNum = input.getValue();
 			mobNum = mobNum.replace(/[^\d]/g, '');
 			input.setValue(mobNum);
+		},
+		onMobNumberChange: function (oEvent) {
+			var input = oEvent.getSource(),
+			    mobNum = input.getValue();
+			if(mobNum.length > 10){
+				MessageToast.show("Upto 10 digits only!");
+				input.setValue(oEvent.getParameters().value.substr(0,10).trim());
+			} 
+			else{
+				input.setValue(mobNum);
+			}
+			if(mobNum.indexOf('.')!=-1){
+				MessageToast.show("No decimal values please!");
+				input.setValue(mobNum.substring(0,mobNum.length-1));
+			}
+			if(isNaN(mobNum)){
+				MessageToast.show("Only numbers please!");
+				input.setValue(mobNum.substring(0,mobNum.length-1));
+			}
 		}
 	});
 });
